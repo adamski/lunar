3cf0793daea63c023d3dedad3388ac30cf90eabc
François-Xavier Thomas <fx.thomas@gmail.com>
2012-12-10 23:35:10 +0100

    Added bottom layout

--- src/main/scala/MainActivity.scala
+++ src/main/scala/MainActivity.scala
@@ -7,7 +7,5 @@ class MainActivity extends Activity with TypedActivity {
   override def onCreate(bundle: Bundle) {
     super.onCreate(bundle)
     setContentView(R.layout.main)
-
-    findView(TR.textview).setText("hello, world!")
   }
 }
